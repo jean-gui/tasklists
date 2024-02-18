af3d8c4b1f5f96df924b4aaca527bab855c83128
Jean-Guilhem Rouel <jean-gui@w3.org>
2024-02-18 15:28:50 +0100

    migrations

--- migrations/Version20240218130316.php
+++ migrations/Version20240218130316.php
@@ -20,7 +20,7 @@ final class Version20240218130316 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE task ADD COLUMN alley VARCHAR(1000) DEFAULT NULL');
+        $this->addSql('ALTER TABLE task ADD COLUMN alley VARCHAR(1000) NOT NULL DEFAULT \'Other\'');
     }
 
     public function down(Schema $schema): void
